4f5f534a4ed997de00c7f3107c8cd684ccc45e03
Filiz Yaraşlıoğlu <36275094+Filizz@users.noreply.github.com>
2022-03-04 21:26:06 +0300

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,21 @@
+#Exercise 1.2: Scripts
+
+print(3 + 1)
+print(3 * 3)
+print(2 ** 3)
+print("Hello, world!")
+
+
+
+#Exercise 1.7: Variables
+
+str = 'John Doe'
+print("Hello, "+str+"!")
+
+
+        
+
+
+
+ 
+
